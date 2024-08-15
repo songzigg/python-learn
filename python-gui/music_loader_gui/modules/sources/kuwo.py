988487c8015c8db8a27aece2afc89aee9f5d876d
songzi <470472264@qq.com>
2024-08-15 23:11:09 +0800

    酷我音乐搜索下载接口更新

--- python-gui/music_loader_gui/modules/sources/kuwo.py
+++ python-gui/music_loader_gui/modules/sources/kuwo.py
@@ -26,19 +26,29 @@ class Kuwo(Base):
         self.headers['csrf'] = token
         cfg = self.config.copy()
         params = {
-            'key': keyword,
+            'all': keyword,
             'pn': str(cfg.get('page', 1)),
+            'vipver': 1,
+            'client': 'kt',
+            'ft': 'music',
+            'cluster': 0,
+            'strategy': 2012,
+            'encoding': 'utf8',
+            'rformat': 'json',
+            'mobi': 1,
+            'show_copyright_off': 1,
+            'issubtitle': 1,
             'rn': cfg['search_size_per_source'],
         }
         response = self.session.get(self.search_url, headers=self.headers, params=params, cookies=cookies)
-        all_items = response.json()['data']['list']
+        all_items = response.json()['abslist']
         songinfos = []
         for item in all_items:
             for br in ['320kmp3', '192kmp3', '128kmp3']:
                 params = {
                     'format': 'mp3',
                     'br': br,
-                    'rid': str(item['rid']),
+                    'rid': str(item['DC_TARGETID']),
                     'type': 'convert_url',
                     'response': 'url',
                 }
@@ -47,9 +57,10 @@ class Kuwo(Base):
                 break
             if not (download_url.startswith('http://') or download_url.startswith('https://')): continue
             params = {
-                'musicId': str(item['rid'])
+                'musicId': str(item['DC_TARGETID']),
+                'httpsStatus': 1
             }
-            self.lyric_headers.update({'Referer': f'http://m.kuwo.cn/yinyue/{str(item["rid"])}'})
+            self.lyric_headers.update({'Referer': f'http://m.kuwo.cn/yinyue/{str(item['DC_TARGETID'])}'})
             response = self.session.get(self.lyric_url, headers=self.lyric_headers, params=params)
             lyric = response.json().get('data', {}).get('lrclist', '')
             filesize = '-MB'
@@ -57,12 +68,12 @@ class Kuwo(Base):
             duration = int(item.get('duration', 0))
             songinfo = {
                 'source': self.source,
-                'songid': str(item['rid']),
-                'singers': filterBadCharacter(item.get('artist', '-')),
-                'album': filterBadCharacter(item.get('album', '-')),
-                'songname': filterBadCharacter(item.get('name', '-')),
+                'songid': str(item['DC_TARGETID']),
+                'singers': filterBadCharacter(item.get('ARTIST', '-')),
+                'album': filterBadCharacter(item.get('ALBUM', '-')),
+                'songname': filterBadCharacter(item.get('NAME', '-')),
                 'savedir': cfg['savedir'],
-                'savename': filterBadCharacter(item.get('name', f'{keyword}_{int(time.time())}')),
+                'savename': filterBadCharacter(item.get('NAME', f'{keyword}_{int(time.time())}')),
                 'download_url': download_url,
                 'lyric': lyric,
                 'filesize': filesize,
@@ -86,6 +97,7 @@ class Kuwo(Base):
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
             'Referer': 'http://m.kuwo.cn/yinyue/'
         }
-        self.search_url = 'http://www.kuwo.cn/api/www/search/searchMusicBykeyWord'
+        # self.search_url = 'http://www.kuwo.cn/api/www/search/searchMusicBykeyWord'
+        self.search_url = 'http://www.kuwo.cn/search/searchMusicBykeyWord'
         self.player_url = 'http://antiserver.kuwo.cn/anti.s'
         self.lyric_url = 'http://m.kuwo.cn/newh5/singles/songinfoandlrc'
